d5b909ff531714c109e1e749ca15c2fb12d76cfc
Taki <taki.gaoqi@outlook.com>
2018-06-05 18:36:57 +0800

    [update] proxy;

--- mock/index.js
+++ mock/index.js
@@ -1,7 +1,7 @@
 // const mockjs = require('mockjs');
 
 module.exports = {
-  // 'GET /api/demo': 'http://127.0.0.1:3000',
+  // 'PROXY /api/demo': 'http://127.0.0.1:3000',
   'GET /api/demo': (req, res) => (
     res.json({
       errno: 0,
--- scripts/utils/mock.js
+++ scripts/utils/mock.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const METHOD = ['get', 'post', 'put', 'delete'];
+const METHOD = ['get', 'post', 'put', 'delete', 'proxy'];
 const VALUE_TYPE = ['function', 'object', 'string'];
 
 function parseKey(key) {
@@ -18,23 +18,12 @@ function parseKey(key) {
 }
 
 function createMockHandler(method, path, value) {
-  return function mockHandler(...args) {
-    const res = args[1];
+  return function mockHandler(req, res, next) {
     if (typeof value === 'function') {
-      value(...args);
+      value(req, res, next);
     } else {
       res.json(value);
     }
-  };
-}
-
-function createProxy(devServer, path, target) {
-  devServer.proxy = {
-    ...devServer.proxy,
-    [path]: {
-      target,
-      changeOrigin: true,
-    },
   };
 }
 
@@ -60,15 +49,27 @@ module.exports = function (devServer, mockFile, customBefore) {
     if (VALUE_TYPE.indexOf(valueType) < 0) {
       throw new Error(`mock value of ${key} should be function or object or string, but got ${valueType}`);
     }
+    if (method === 'proxy') {
+      const proxyValue = valueType === 'string' ? {
+        target: value,
+        changeOrigin: true,
+      } : valueType === 'object' ? value : value();
+
+      devServer.proxy = {
+        ...devServer.proxy,
+        [path]: proxyValue,
+      };
+      return true;
+    }
     if (valueType === 'string') {
-      createProxy(devServer, path, value);
+      throw new Error(`mock value of ${key} should be function or object, but got string`);
     }
   })
 
   return function(app) {
     mockKeys.forEach(key => {
       const { method, path } = parseKey(key);
-      if (typeof mockConfig[key] === 'string') {
+      if (method==='proxy') {
         return false;
       }
       return app[method](path, createMockHandler(method, path, mockConfig[key]));
