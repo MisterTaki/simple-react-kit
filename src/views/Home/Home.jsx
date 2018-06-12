e6082babeda7f558759c4355710fb41dc05554b2
Taki <taki.gaoqi@outlook.com>
2018-06-12 11:55:49 +0800

    [update] classnames/bind;

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
 import logo from '@/assets/logo.svg';
-import './Home.less';
+import styles from './Home.less';
 
-const Home = ({ demo }) => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+// https://github.com/JedWatson/classnames#alternate-bind-version-for-css-modules
+const cls = classNames.bind(styles);
+
+const Home = ({ className, demo }) => (
+  <div className={cls('App', className)}>
+    <header className={cls('App-header')}>
+      <img src={logo} className={cls('App-logo')} alt="logo" />
+      <h1 className={cls('App-title')}>Welcome to React</h1>
     </header>
-    <p className="App-intro">
+    <p className={cls('App-intro')}>
       To get started, edit <code>src/App.jsx</code> and save to reload.
     </p>
     <button onClick={demo}>to Demo</button>
